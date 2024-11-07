a3e182fdeaa7bffd4db219d3fbe06f28f1effa38
MARGAM VINAY <margamvinay77@gmail.com>
2024-11-07 18:29:33 +0530

    one-to-one message feature

--- rechatBackend/src/config/db.ts
+++ rechatBackend/src/config/db.ts
@@ -2,12 +2,9 @@ import mongoose from "mongoose";
 // import { Error } from "mongoose";
 export default async function connectDB() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://margamvinay:vinay756@vinay.tch0oea.mongodb.net/?retryWrites=true&w=majority&appName=vinay",
-      {
-        dbName: "RealTimeChat",
-      }
-    );
+    await mongoose.connect("", {
+      dbName: "RealTimeChat",
+    });
   } catch (error) {
     process.exit(1);
   }
